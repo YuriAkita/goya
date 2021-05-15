40f1d471e4526225fdcb9a7a974ad4a3efffcdbd
akitayuri <nmnl_y.bgux@icloud.com>
2021-05-15 19:47:35 +0900

    課題途中

--- goya.rb
+++ goya.rb
@@ -2,6 +2,7 @@
 require 'cgi'
 cgi = CGI.new
 # データを受け取った後、HTMLの形式でレスポンスを返す
+if cgi.include?('goya')
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
   # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
   get = cgi['goya']
@@ -13,3 +14,24 @@ cgi.out("type" => "text/html", "charset" => "UTF-8") {
     </body>
   </html>"
 }
+elsif cgi.include?('goya_2')
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+get = cgi['goya']
+  "<html>
+    <body>
+      <p>品質が悪いもの（false）のgoyaの情報は下記になります</p>
+      文字列：#{get}
+    </body>
+  </html>"
+}
+elsif cgi.include?('goya_3')
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+  get = cgi['goya']
+  "<html>
+    <body>
+      <p>譲渡先が自家消費ではないもののgoyaの情報は下記になります</p>
+      文字列：#{get}
+    </body>
+  </html>"
+}
+end
--- sample_pg.rb
+++ sample_pg.rb
@@ -14,6 +14,23 @@ begin
       # 各行を取り出し、putsでターミナル上に出力する
       puts "ゴーヤの大きさ：#{record["weight"]}　売った相手：#{record["give_for"]}"
   end
+
+  result_2 = connection.exec("SELECT * FROM crops WHERE quality = false;")
+  # 取り出した各行を処理する
+  quality_result.each do |record_2|
+      # 各行を取り出し、putsでターミナル上に出力する
+      puts "ゴーヤの長さ：#{record_2["length"]}　ゴーヤの大きさ：#{record_2["weight"]}　ゴーヤの品質：#{record_2["quality"]}　
+      売った相手：#{record_2["give_for"]}　年月日：#{record_2["date"]}"
+  end
+
+  result_3 = connection.exec("SELECT * FROM crops WHERE give_for != '自家消費';")
+  # 取り出した各行を処理する
+  result_3.each do |record_3|
+      # 各行を取り出し、putsでターミナル上に出力する
+      puts "ゴーヤの長さ：#{record_3["length"]}　ゴーヤの重さ：#{record_3["weight"]}　ゴーヤの品質：#{record_3["quality"]}　
+      売った相手：#{record_3["give_for"]}　年月日：#{record_3["date"]}"
+  end
+
 ensure
   # 最後に.finishでデータベースへのコネクションを切断する
   connection.finish
